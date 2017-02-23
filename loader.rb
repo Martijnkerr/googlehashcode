dd4a4f9ab49e445ddfc2f71df09fea9b8678656b
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2017-02-23 19:33:14 +0100

    Reading latencies data

--- loader.rb
+++ loader.rb
@@ -1,11 +1,24 @@
 class Loader
+  attr_reader :latencies
+
   def initialize(file_path)
     @file_path = file_path
   end
 
   def load
     file = File.open(@file_path, 'r')
-    videos, endpoints, requests, caches, capacity = file.readline.split.map(&:to_i)
+    @videos, @endpoints, @requests, @caches, @capacity = file.readline.split.map(&:to_i)
     sizes = file.readline.split.map(&:to_i)
+
+    @latencies = []
+    @endpoints.times do
+      data_center_latency, cache_servers = file.readline.split.map(&:to_i)
+      endpoint_lats = { dc: data_center_latency }
+      cache_servers.times do
+        id, latency = file.readline.split.map(&:to_i)
+        endpoint_lats[id] = latency
+      end
+      @latencies << endpoint_lats
+    end
   end
 end
--- runner.rb
+++ runner.rb
@@ -2,4 +2,4 @@ require './loader'
 require 'byebug'
 
 loader = Loader.new(ARGV[0])
-data = loader.load
+loader.load
